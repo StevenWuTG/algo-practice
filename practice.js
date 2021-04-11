87c99633bd2f2997ed4c2a3536c14c9219bf1669
StevenWuTG <stevenwu91@icloud.com>
2021-04-11 15:33:14 -0400

    first question done

--- practice.js
+++ practice.js
@@ -1,9 +1,18 @@
 //write a function which takes in a string an returns counts of each character in the string.
 
-const charCount = () => {
-    //make object to return at end
-    //loop over string
-    //return object
+const charCount = (string) => {
+    var result = {}
+    for (var i = 0; i < string.length; i++){
+        var char = string[i]
+        if (result[char] > 0){
+            result[char]++
+        }
+        else {
+            result[char] = 1
+        }
+    }
+    return console.log(result)
+
 }
 
 
