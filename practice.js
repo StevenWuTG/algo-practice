460e09bde3264fe7f2ac6214654f2feeaa85d7a4
StevenWuTG <stevenwu91@icloud.com>
2021-04-11 15:27:50 -0400

    first commit

---
+++ practice.js
@@ -0,0 +1,10 @@
+//write a function which takes in a string an returns counts of each character in the string.
+
+const charCount = () => {
+    //make object to return at end
+    //loop over string
+    //return object
+}
+
+
+charCount("hello") //{h:1, e:1, l:2, o:1}
