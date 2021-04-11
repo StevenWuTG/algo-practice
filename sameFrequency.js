0e88e37e18cd57e19d2cba7ee19cc941d3e76801
StevenWuTG <stevenwu91@icloud.com>
2021-04-11 15:43:43 -0400

    sameFrequency counter solved

---
+++ sameFrequency.js
@@ -0,0 +1,30 @@
+//write a function called sameFrequency, given two postice integers find out if the teo numbers have the same frequency of digits, solution musht have Time complexity of O(N)
+
+
+function sameFrequency(num1, num2){
+    let strNum1 = num1.toString();
+    let strNum2 = num2.toString();
+    if(strNum1.length !== strNum2.length) return console.log(false);
+    
+    let countNum1 = {};
+    let countNum2 = {};
+    
+    for(let i = 0; i < strNum1.length; i++){
+      countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
+    }
+    
+    for(let j = 0; j < strNum1.length; j++){
+      countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
+    }
+    
+    for(let key in countNum1){
+      if(countNum1[key] !== countNum2[key]) return console.log(false);
+    }
+   
+    return console.log(true);
+}
+
+
+// sameFrequency(182,281) //true
+
+sameFrequency(31,14) //false
