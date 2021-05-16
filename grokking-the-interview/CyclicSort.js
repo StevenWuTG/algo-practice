7fa8fcf9065622d76ac57a4682db41124e36f5ec
StevenWuTG <stevenwu91@icloud.com>
2021-05-16 19:06:21 -0400

    finished Cyclic sort q1

---
+++ grokking-the-interview/CyclicSort.js
@@ -0,0 +1,27 @@
+//We are given an array containing ‘n’ objects. Each object, when created, was assigned a unique number from 1 to ‘n’ based on their creation sequence. This means that the object with sequence number ‘3’ was created just before the object with sequence number ‘4’.
+
+//Write a function to sort the objects in-place on their creation sequence number in O(n)O(n) and without any extra space. For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
+
+//EXAMPLE
+//Input: [3, 1, 5, 4, 2]
+//Output: [1, 2, 3, 4, 5]
+
+const cyclic_sort = (nums) => {
+    let i = 0;
+    while (i < nums.length) {
+        const j = nums[i] - 1;
+        if (nums[i] !== nums[j]) {
+            [nums[i], nums[j]] = [nums[j], nums[i]]; // swap
+        } else {
+            i += 1;
+        }
+    }
+    console.log(nums)
+    return nums;
+}
+  
+  
+console.log(cyclic_sort([3, 1, 5, 4, 2]));
+console.log(cyclic_sort([2, 6, 4, 3, 1, 5]));
+console.log(cyclic_sort([1, 5, 6, 4, 3, 2]));
+  
